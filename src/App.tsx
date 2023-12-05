a16d6143409ac21a7df685ed76c0f365787e97f6
Your Name <example@gmail.com>
2023-12-05 14:53:58 +0200

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { StyledEngineProvider } from "@mui/material";
 import { FC } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import ErrorFallback from "./components/ErrorFallback";
 import Router from "./components/Router";
@@ -10,14 +10,14 @@ import "./App.css";
 
 const App: FC = () => (
   <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <BrowserRouter>
+    <HashRouter>
       <PreventExitContext>
         <StyledEngineProvider injectFirst>
           <NavBar />
           <Router />
         </StyledEngineProvider>
       </PreventExitContext>
-    </BrowserRouter>
+    </HashRouter>
   </ErrorBoundary>
 );
 
